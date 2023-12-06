6cbe0fb8dd3cf74309edd4cb9c0c21b61eb749d2
h-azuma <azhide29@gmail.com>
2023-12-06 21:18:14 +0900

    Initial commit

---
+++ test/pull-request-slack-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PullRequestSlackApp from '../lib/pull-request-slack-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/pull-request-slack-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PullRequestSlackApp.PullRequestSlackAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
